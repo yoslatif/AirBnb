a431da05049b0d70714c43781968fd338b929631
Yoseph <yoslatif@gmail.com>
2023-08-08 02:21:12 -0700

    bunchastuff

---
+++ backend/routes/index.js
@@ -0,0 +1,25 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+module.exports = router;
+
+
+// backend/app.js
+const routes = require('./routes');
+
+// ...
+
+app.use(routes); // Connect all the routes
+
+
+
+// backend/app.js
+// ...
+
+module.exports = app;
