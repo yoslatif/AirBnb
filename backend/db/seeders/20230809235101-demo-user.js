e1ff6a38a29a8983b60b403038ef12293d43329b
Yoseph <yoslatif@gmail.com>
2023-08-11 18:26:37 -0700

    seeder

--- backend/db/seeders/20230809235101-demo-user.js
+++ backend/db/seeders/20230809235101-demo-user.js
@@ -10,26 +10,37 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
-      {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
-      },
-      {
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
-      },
-      {
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
-      }
-    ], { validate: true });
-  },
+    try {
+      await User.bulkCreate([
+        {
+          firstName: 'demo',
+          lastName: 'lition',
+          email: 'demo@user.io',
+          username: 'Demo-lition',
+          hashedPassword: bcrypt.hashSync('password', 10)
+        },
+        {
+          firstName: 'fake',
+          lastName: 'usa',
+          email: 'user1@user.io',
+          username: 'FakeUser1',
+          hashedPassword: bcrypt.hashSync('password2', 10)
+        },
+        {
+          firstName: 'fakak',
+          lastName: 'coco',
+          email: 'user2@user.io',
+          username: 'FakeUser2',
+          hashedPassword: bcrypt.hashSync('password3', 10)
+        }
+      ], { validate: true });
+    } catch (error) {
+      console.error("Error seeding users: ", error);
+      throw error;
+  }
+},
 
-  async down (queryInterface, Sequelize) {
+async down (queryInterface, Sequelize) {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
