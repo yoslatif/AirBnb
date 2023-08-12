f7705741fef3fb8453213435ece0bb69f22f8b91
Yoseph <yoslatif@gmail.com>
2023-08-11 19:07:48 -0700

    hahahah

--- backend/db/migrations/20230812020353-createUser.js
+++ backend/db/migrations/20230812020353-createUser.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let options = {snake_schema};
+let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
