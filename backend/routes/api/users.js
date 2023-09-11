72c6095f6e7337d6c4c3eb8f51025b337f56c2a6
Yoseph <yoslatif@gmail.com>
2023-09-11 01:30:32 -0700

    daybefore

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,11 +5,16 @@ const { validateSignup, analyzeErrors } = require('../api/validators.js');
 
 const router = express.Router();
 
-router.post('/', validateSignup, async (req, res) => {
-    analyzeErrors(req, res, async () => {
-        const { email, password, username, firstName, lastName } = req.body;
-        const user = await User.signup({ email, username, password, firstName, lastName });
 
+// POST route handler for user signup
+router.post('/', validateSignup, async (req, res) => {
+    // Analyzes any validation errors and proceeds with user signup if validation succeeds
+    analyzeErrors(req, res, async () => {
+        // Destructures necessary fields from the request body
+        const { email, password, username, firstName, lastName } = req.body;
+        // Signs up a new user with the given details
+        const user = await User.signup({ email, username, password, firstName, lastName });
+        // Sets a token cookie and sends a response with the user's safe object and token
         const token = setTokenCookie(res, user);
 
         return res.json({ ...user.toSafeObject(), token });
