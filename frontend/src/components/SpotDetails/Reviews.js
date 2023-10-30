322a907105f89f152b22376871b4b073209f8cce
Yoseph <yoslatif@gmail.com>
2023-10-30 07:35:45 -0700

    new1

--- frontend/src/components/SpotDetails/Reviews.js
+++ frontend/src/components/SpotDetails/Reviews.js
@@ -6,7 +6,7 @@ import "./Reviews.css";
 export default function Reviews({ spot, reviews }) {
     const user = useSelector(state => state.session.user);
     if (!reviews) return;
-    const userReviewed = user && reviews.some(review => review.User.id === user.id);
+    const userReviewed = user && reviews.some(review => review?.User.id === user.id);
     const userOwnsSpot = user && spot.ownerId === user.id;
     return <div>
         <ReviewHeader user={user} reviews={reviews} spot={spot} userReviewed={userReviewed} userOwnsSpot={userOwnsSpot} />
