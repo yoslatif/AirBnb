3a9a2d1c015d8e3526a4a623948c35ceb720b0ab
yoslatif <yoslatif@gmail.com>
2024-05-05 16:50:59 -0700

    somewhatfixedsearch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
+// App.js
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-
 import { restoreUser } from "./store/session";
 import SpotGrid from "./components/SpotGrid/SpotGrid";
 import SpotDetails from "./components/SpotDetails/SpotDetails";
@@ -11,32 +11,35 @@ import Header from "./components/Header/Header";
 export default function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-      dispatch(restoreUser());
+    dispatch(restoreUser());
   }, [dispatch]);
 
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = (term) => {
-      setSearchTerm(term);
+    setSearchTerm(term);
+    console.log("Search term set to:", term); // For debugging
   };
 
   return (
-      <div>
-          <Modals />
-          <Switch>
-              <Route exact path="/">
-                  {/* Pass handleSearch function as onSearch prop to Header */}
-                  <Header className="homepageHeader" onSearch={handleSearch} />
-                  <SpotGrid searchTerm={searchTerm} />
-              </Route>
-              <Route path="/spots/:spotId"><SpotDetails /></Route>
-              <Route path="/spotsgrid">
-                  {/* Pass handleSearch function as onSearch prop to Header */}
-                  <Header className="homepageHeader" onSearch={handleSearch} />
-                  <SpotGrid searchTerm={searchTerm} />
-              </Route>
-              <Route path="*"><SpotDetails /></Route>
-          </Switch>
-      </div>
+    <div>
+      <Modals />
+      <Switch>
+        <Route exact path="/">
+          <Header className="homepageHeader" onSearch={handleSearch} />
+          <SpotGrid searchTerm={searchTerm} />
+        </Route>
+        <Route path="/spots/:spotId">
+          <SpotDetails />
+        </Route>
+        <Route path="/spotsgrid">
+          <Header className="homepageHeader" onSearch={handleSearch} />
+          <SpotGrid searchTerm={searchTerm} />
+        </Route>
+        <Route path="*">
+          <SpotDetails />
+        </Route>
+      </Switch>
+    </div>
   );
 }
